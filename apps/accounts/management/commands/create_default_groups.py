ba199c9853e93b526e9d02b48da2c42b6bf0de2c
Alan Viars <aviars@videntity.com>
2018-11-13 09:04:38 -0500

    Add a management command to great default groups/roles.

---
+++ apps/accounts/management/commands/create_default_groups.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import Group
+from django.core.management.base import BaseCommand
+
+__author__ = "Alan Viars"
+
+
+def create_groups():
+    groups = ["ChangeIdentityAssuranceLevel",
+              "ApproveOrganizationalUserAccount",
+              "RegisterOAuth2ClientApps"]
+    created_groups = []
+    for group in groups:
+        g, created = Group.objects.get_or_create(name=group)
+        created_groups.append(created)
+    return dict(zip(groups, created_groups))
+
+
+class Command(BaseCommand):
+    help = 'Create Groups. Run only 1x at initial setup.'
+
+    def handle(self, *args, **options):
+        create_groups()
