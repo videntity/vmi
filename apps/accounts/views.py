db6026ed462c28ff5585439684b8f155aa4fb69e
David Gage <whytheplatypus@gmail.com>
2019-02-20 16:04:56 -0500

    use default for request get parameter

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -113,7 +113,7 @@ def create_account(request, service_title=settings.APPLICATION_TITLE):
             # authenticate user then login
             user = authenticate(username=username, password=password)
             login(request, user)
-            redirect_url = request.GET['next'] if request.GET['next'] else reverse('home')
+            redirect_url = request.GET.get('next', reverse('home'))
             return HttpResponseRedirect(redirect_url)
         else:
             # return the bound form with errors
