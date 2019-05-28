0e5f96f2d04b206fa13228d607b2dd9f02fe5f33
David Gage <whytheplatypus@gmail.com>
2019-05-28 10:44:15 -0400

    Configure MEDIA storage for s3

--- vmi/settings.py
+++ vmi/settings.py
@@ -146,7 +146,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME", "development-vmi-media-storage")
+AWS_AUTO_CREATE_BUCKET = True
+AWS_S3_FILE_OVERWRITE = False
+DEFAULT_FILE_STORAGE = env("DEFAULT_FILE_STORAGE", 'django.core.files.storage.FileSystemStorage')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
