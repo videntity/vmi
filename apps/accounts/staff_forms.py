fe9e14a13254d669c9bb9738782d41df30d0a468
Alan Viars <aviars@videntity.com>
2019-09-03 17:29:29 -0400

    remove sex, gender identity, and birthdate for OrgAgent staff signup.

--- apps/accounts/staff_forms.py
+++ apps/accounts/staff_forms.py
@@ -7,7 +7,6 @@ from django.contrib.auth.password_validation import validate_password
 from .models import UserProfile, create_activation_key, Organization, OrganizationAffiliationRequest
 from django.conf import settings
 from django.utils.safestring import mark_safe
-from .models import SEX_CHOICES, GENDER_CHOICES
 
 # Copyright Videntity Systems Inc.
 YEARS = [x for x in range(1901, 2000)]
@@ -25,20 +24,13 @@ class StaffSignupForm(forms.Form):
     email = forms.EmailField(max_length=150, label=_("Email*"), required=True)
     first_name = forms.CharField(max_length=100, label=_("First Name*"))
     last_name = forms.CharField(max_length=100, label=_("Last Name*"))
-    miiddle_name = forms.CharField(
+    middle_name = forms.CharField(
         max_length=100, label=_("Middle Name"), required=False)
     nickname = forms.CharField(
         max_length=100, label=_("Nickname"), required=False)
     mobile_phone_number = forms.CharField(required=True,
                                           label=_("Mobile Phone Number*"),
                                           max_length=10)
-    sex = forms.ChoiceField(choices=SEX_CHOICES, required=False,
-                            help_text="Enter sex, not gender identity.")
-    gender_identity = forms.ChoiceField(choices=GENDER_CHOICES, required=False,
-                                        help_text="Gender identity is not necessarily the same as birth sex.")
-    birth_date = forms.DateField(
-        label='Birth Date', widget=forms.SelectDateWidget(years=YEARS), required=False)
-
     password1 = forms.CharField(widget=forms.PasswordInput, max_length=128,
                                 label=_("Password*"))
     password2 = forms.CharField(widget=forms.PasswordInput, max_length=128,
@@ -114,7 +106,7 @@ class StaffSignupForm(forms.Form):
         return self.cleaned_data.get("last_name", "").strip().upper()
 
     def clean_middle_name(self):
-        return self.cleaned_data.get("last_name", "").strip().upper()
+        return self.cleaned_data.get("middle_name", "").strip().upper()
 
     def clean_nickname(self):
         return self.cleaned_data.get("nickname", "").strip().upper()
@@ -135,8 +127,6 @@ class StaffSignupForm(forms.Form):
             nickname=self.cleaned_data.get('nickname', ''),
             middle_name=self.cleaned_data.get('middle_name', ""),
             mobile_phone_number=self.cleaned_data['mobile_phone_number'],
-            sex=self.cleaned_data.get('sex', ""),
-            gender_identity=self.cleaned_data.get('gender_identity', ""),
             agree_tos=settings.CURRENT_TOS_VERSION,
             agree_privacy_policy=settings.CURRENT_PP_VERSION)
         up.save()
