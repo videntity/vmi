2ad3f7ce49b6ae706cfa2222af16d786f13c0c59
Alan Viars <aviars@videntity.com>
2019-09-12 17:29:18 -0400

    Password reset (#197)
    * added country and state to individual IDs.
    
    * comment.
    
    * update requirements.
    
    * add django-dotenv
    
    * focus on password when username already supplied.
    
    * prepoulate username when URL contains ?username=foo
    
    * typo fix
    
    * Update script to accept all possible variables
    
    * Make email optioonal to create password reset key
    
    * Password reset with passphrase
    
    * fixed typos

--- apps/accounts/password_recovery_passphrase_generator.py
+++ apps/accounts/password_recovery_passphrase_generator.py
@@ -1,7 +1,26 @@
 from mnemonic import Mnemonic
+from django.utils.crypto import pbkdf2
+import binascii
+from django.conf import settings
 
 
 def generate_password_recovery_phrase():
     m = Mnemonic('english')
     mywords = m.generate(256)
     return "%s" % (mywords)
+
+
+def get_passphrase_salt(salt=settings.PASSPHRASE_SALT):
+    """
+    Assumes `USER_ID_SALT` is a hex encoded value. Decodes the salt val,
+    returning binary data represented by the hexadecimal string.
+    :param: salt
+    :return: bytes
+    """
+    return binascii.unhexlify(salt)
+
+
+def passphrase_hash(passphrase):
+    return binascii.hexlify(pbkdf2(passphrase,
+                                   get_passphrase_salt(),
+                                   settings.PASSPHRASE_ITERATIONS)).decode("ascii")
