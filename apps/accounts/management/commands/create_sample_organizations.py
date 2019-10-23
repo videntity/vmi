68ff9b70c8bfdd966b79f75baa1d547ebe6a17cf
Alan Viars <aviars@videntity.com>
2019-10-23 09:38:33 -0400

    Org agent onboarding improvements (#228)
    * added country and state to individual IDs.
    
    * comment.
    
    * update requirements.
    
    * Update email contents for Org agent signup
    
    * settings update
    
    * add profile to org agent dashboard.
    
    * Removing command.
    
    * Removing command. flake8

--- apps/accounts/management/commands/create_sample_organizations.py
+++ apps/accounts/management/commands/create_sample_organizations.py
@@ -1,41 +1,17 @@
 from django.core.management.base import BaseCommand
-from ...models import Organization
-from django.contrib.auth.models import Group
-
-__author__ = "Alan Viars"
-
-
-organizations = [{"name": "CBO XYZ", "slug": "cbo-xyz"},
-                 {"name": "ACME Health", "slug": "acme-health"},
-                 {"name": "Onboarding", "slug": "onboarding"},
-                 ]
-
-default_groups_for_org_agents = [
-    'OrganizationalAgent', 'IdentityAssuranceTrustedReferee']
 
 
 def create_sample_organizations():
-
-    created_orgs = []
-    for organization in organizations:
-
-        o, created = Organization.objects.get_or_create(name=organization['name'],
-                                                        slug=organization['slug'])
-
-        for g in default_groups_for_org_agents:
-            group, created = Group.objects.get_or_create(name=g)
-            o.default_groups_for_agents.add(group)
-            o.save()
-
-        created_orgs.append(o)
-
-    return created_orgs
+    # Pass - removing this command because is
+    # was causing errors and can be done manually.
+    # problematic and unnecessary.
+    return []
 
 
 class Command(BaseCommand):
-    help = 'Create sample organizations. %s' % (organizations)
+
+    help = 'Create sample organizations.'
 
     def handle(self, *args, **options):
-
         orgs = create_sample_organizations()
         print("Organizations %s created if they did not already exist." % (orgs))
