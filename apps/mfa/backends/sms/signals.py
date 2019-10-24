8b37df8e711a25c68f884d5fa6bc3e1b2adbb26c
Alan Viars <aviars@videntity.com>
2019-10-24 13:16:52 -0400

    pep8

--- apps/mfa/backends/sms/signals.py
+++ apps/mfa/backends/sms/signals.py
@@ -2,12 +2,14 @@ import boto3
 from django.db.models.signals import post_save
 from django.conf import settings
 
+
 def send_sms_code(sender, instance, created, **kwargs):
     sns = boto3.client('sns')
     number = instance.device.phone_number
     sns.publish(
         PhoneNumber=number,
-        Message="Your code for %s is : %s" % (settings.ORGANIZATION_NAME, instance.code),
+        Message="Your code for %s is : %s" % (
+            settings.ORGANIZATION_NAME, instance.code),
         MessageAttributes={
             'AWS.SNS.SMS.SenderID': {
                 'DataType': 'String',
