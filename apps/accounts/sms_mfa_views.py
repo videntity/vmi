1c63af1eaf1f2c0ad091e8db8ce50f02975c6701
Alan Viars <aviars@videntity.com>
2019-10-25 16:58:53 -0400

    remove old code.

--- apps/accounts/sms_mfa_views.py
+++ apps/accounts/sms_mfa_views.py
@@ -1,5 +1,5 @@
 import logging
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login
@@ -16,67 +16,6 @@ from ratelimit.decorators import ratelimit
 __author__ = "Alan Viars"
 
 logger = logging.getLogger(__name__)
-
-
-# def mfa_code_confirm(request, uid):
-#     mfac = get_object_or_404(MFACode, uid=uid)
-#     user = mfac.user
-#     if request.method == 'POST':
-#         form = MFACodeForm(request.POST)
-#         if form.is_valid():
-#             code = form.cleaned_data['code']
-# 
-#             if code != mfac.code:
-#                 mfac.tries_counter = mfac.tries_counter + 1
-#                 if mfac.tries_counter > 3:
-#                     messages.error(
-#                         request,
-#                         _(
-#                             'Maximum tries reached.'
-#                             'The authentication attempt has been invalidated.'
-#                         ),
-#                     )
-#                     mfac.delete()
-#                 else:
-#                     mfac.save()
-#                 messages.error(
-#                     request,
-#                     _(
-#                         'The code supplied did not match what was sent.'
-#                         'Please try again.'
-#                     ),
-#                 )
-# 
-#                 return render(request, 'generic/bootstrapform.html', {'form': form})
-# 
-#             if user.is_active:
-#                 # Fake backend here since its not needed.
-#                 user.backend = 'django.contrib.auth.backends.ModelBackend'
-#                 # Else, just login as normal without MFA
-#                 login(request, user)
-#                 mfac.delete()
-#                 next_param = request.GET.get('next', '')
-#                 if next_param:
-#                     # If a next is in the URL, then go there
-#                     return HttpResponseRedirect(next_param)
-#                 # otherwise just go to home.
-#                 return HttpResponseRedirect(reverse('home'))
-#             else:
-#                 # The user exists but is_active=False
-#                 messages.error(
-#                     request,
-#                     _(
-#                         'Your account has not been activated.'
-#                         'Please check your email for a link to'
-#                         'activate your account.'
-#                     ),
-#                 )
-#                 return render(request, 'generic/bootstrapform.html', {'form': form})
-# 
-#         else:
-#             return render(request, 'generic/bootstrapform.html', {'form': form})
-#     # this is a GET
-#     return render(request, 'generic/bootstrapform.html', {'form': MFACodeForm()})
 
 
 @never_cache
