e9f285b3ef83137e16cac2f3fc5811fd5e6602d8
Matthew Mosier <Matthew.Mosier@newwave.io>
2020-01-15 14:03:38 -0500

    Fixes: in person proofing only, Health Insurance ID Card bug, ID document verification type bug

--- apps/accounts/staff_forms.py
+++ apps/accounts/staff_forms.py
@@ -19,8 +19,7 @@ agree_tos_label = mark_safe(
     'Do you agree to the <a href="%s" target="_blank">terms of service</a>?' % (settings.AGENT_TOS_URI))
 
 attest_training_completed_label = mark_safe(
-    """Yes, I attest I have completed the <a href="%s" target="_blank">training</a>
-    and will abide by the code of conduct.""" % (settings.TRAINING_URI))
+    """Yes, I attest I have completed the training and will abide by the code of conduct.""" 
 
 
 class StaffSignupForm(forms.Form):
@@ -51,7 +50,7 @@ class StaffSignupForm(forms.Form):
     password2 = forms.CharField(widget=forms.PasswordInput, max_length=128,
                                 label=_("Password (again)*"))
     agree_tos = forms.BooleanField(label=_(agree_tos_label))
-    attest_training_completed = forms.BooleanField(
+    attest_training_completed = forms.BooleanField(required=True,
         label=_(attest_training_completed_label))
     org_slug = forms.CharField(widget=forms.HiddenInput(),
                                max_length=128, required=True)
