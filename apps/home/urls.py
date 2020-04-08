197fd6e4d9c36bd99ae947033e742413897c969d
Alan Viars <aviars@videntity.com>
2020-04-08 13:36:35 -0400

    Custom homepage (#273)
    * added country and state to individual IDs.
    
    * comment.
    
    * update requirements.
    
    * Add profile picture to staff signup form.
    
    * fix merge
    
    * pep8
    
    * Fixing issue #245
    
    * Fixing issue #244
    
    * Fix bug in rendering document claim and userinfo
    
    * fix reversal of account number and username in SMS welcome message.
    
    * Add more human readable IAL evidence choices.
    
    * Add report and control access to report by requiring user to be in the  group
    
    * Add date widget to date fields.
    
    * Add help text explaining  why Sex and Birthdate are collected.
    
    * user only one screen for the homepage.
    
    * code cleanup
    
    * Adding agent tools to nav + User Search
    
    * simplify homepage code.
    
    * simplify homepage code.
    
    * Switch default email domain to example.com

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,19 +1,22 @@
 from django.urls import path
 from django.conf.urls import url
 from django.contrib import admin
-from .views import authenticated_home, select_org_for_account_create
+from .views import home, select_org_for_account_create
 
 __author__ = "Alan Viars"
 
 admin.autodiscover()
 
 urlpatterns = [
-    path(r'', authenticated_home, name='auth_home'),
+    path(r'', home, name='auth_home'),
 
-    # user_type is either "member" or "agent"
-    url("^select-org-for-account-create/(?P<user_type>[^/]+)/$", select_org_for_account_create,
+    # user_type is either "member" or "agent.
+    url("^select-org-for-account-create/(?P<user_type>[^/]+)/$",
+        select_org_for_account_create,
         name='select-org-for-account-create_w_usertype'),
 
+
+    # Account creation defaults to member when no user_type is given.
     path('select-org-for-account-create/',
          select_org_for_account_create,
          name='select-org-for-account-create')
