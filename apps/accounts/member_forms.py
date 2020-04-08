aa53b806e8f320746124e1dbbeb26fa84ade9688
Mark Scrimshire <mscrimshire+github@gmail.com>
2020-04-08 13:55:13 -0400

    fix conflicts in forms

--- apps/accounts/member_forms.py
+++ apps/accounts/member_forms.py
@@ -18,13 +18,9 @@ YEARS = [x for x in range(1901, 2000)]
 
 User = get_user_model()
 
-<<<<<<< HEAD
 agree_tos_label = mark_safe(
-    'Do you agree to the <a href="%s">Terms of Service</a> and <a href="%s">Privacy Policy</a>?' % (settings.TOS_URI, settings.POLICY_URI))
-=======
-agree_tos_label = mark_safe("""Do you agree to the <a href="%s">Terms of Service</a>
-                            and <a href="%s">Privacy Policy</a>?""" % (settings.TOS_URI, settings.POLICY_URI))
->>>>>>> 3cedf3019ac38d95ed70c2cf29a6cf34efe4d7b1
+    'Do you agree to the <a href="%s">Terms of Service</a> and <a href="%s">Privacy Policy</a>?' % (settings.TOS_URI,
+                                                                                                    settings.POLICY_URI))
 
 
 class MemberSignupForm(forms.Form):
