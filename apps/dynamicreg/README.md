73474af4656a1d9528a8146b57b2b89cbe91f475
Alan Viars <aviars@videntity.com>
2020-07-24 16:05:01 -0400

    Add command line tool for registering apps. Add DCRP endpoint for same.

---
+++ apps/dynamicreg/README.md
@@ -0,0 +1,70 @@
+Dynamic Client Registration Protocol and analogous Django Management Command
+============================================================================
+
+
+Dynamic Client Registration Protocol
+------------------------------------
+
+To use Dynamic Client Registration Protocol (DCRP) you must have a valid username and password.
+You must also be added to the `DynamicClientRegistrationProtocol` group by a systems administrator.
+Currently this feature only registers `confidential` clients with the `authorization-code` grant type.
+
+
+Below is a curl example for DCRP
+
+The file `dr.json` contains the bofy of the HTTP request. For example:
+
+
+    {
+    "client_name": "foo",
+    "client_id": "bar",
+    "redirect_uris": ["https://foo.com", "https://bar.com"]
+    }
+    
+The command to issue will look like:
+
+
+    curl -X POST -d @dr.json http://verifymyidentity:8000/dcrp/register --user your-username:your-password
+
+A successfult HTTP result will contain the client key in its body as JSON....
+
+
+    {
+    "client_name": "bar",
+    "client_id": "foo",
+    "redirect_uris": ["https://foo.com", "https://bar.com"],
+    "client_secret": "bnyCgI6HXZg1GaEOwzEJtyItW0MUclobAcrzwiKOICdVaD.....",
+    "grant_types": ["authorization-code", "refresh_token"]
+    }
+    
+    
+Please also check out the tests for more examples.
+
+Django Management Command: register_oauth2_client
+------------------------------------------------
+
+
+This command will create or modify applications (e.g.  replying parties)
+on the command line. At least one entry is needed for redirect_uris.
+The examples illustrate two.
+
+
+Use the `register_oauth2_client` command like so:
+
+
+    python manage.py  register_oauth2_client CLIENT_ID CLIENT_NAME --redirect_uris https://example.com/callback1 https://foo.example.com/callback2
+
+
+If you want to specify the skip authorization option, issue the command like so:
+
+
+    python manage.py  register_oauth2_client CLIENT_ID CLIENT_NAME --redirect_uris https://example.com/callback1 https://foo.example.com/callback2 --skip_authorization 1
+
+
+    python manage.py  register_oauth2_client CLIENT_ID CLIENT_NAME --redirect_uris https://example.com/callback1 https://foo.example.com/callback2
+    
+    
+You may also use this command to delete an application.
+
+
+    python manage.py register_oauth2_client CLIENT_ID --delete
---
+++ apps/dynamicreg/management/commands/register_oauth2_client.py
@@ -0,0 +1,72 @@
+from django.core.management.base import BaseCommand, CommandError
+from oauth2_provider.models import Application
+from collections import OrderedDict
+import json
+import uuid
+
+__author__ = "Alan Viars"
+
+
+def delete_app(client_id):
+    try:
+        application = Application.objects.get(client_id=client_id)
+        application.delete()
+        return application
+    except Application.DoesNotExist:
+        raise CommandError(
+            "Application %s not deleted because it doesn't exist." % (client_id))
+
+
+def register_app(client_id, client_name, redirect_uris=[],
+                 skip_authorization=False):
+
+    dynreg = OrderedDict()
+    dynreg['client_name'] = client_name
+    dynreg['client_id'] = client_id
+    dynreg['redirect_uris'] = redirect_uris
+    application, g_o_c = Application.objects.get_or_create(
+        client_id=dynreg['client_id'])
+    application.name = dynreg['client_name']
+    application.redirect_uris = dynreg['redirect_uris']
+    application.client_type = 'confidential'
+    application.authorization_grant_type = 'authorization-code'
+    application.skip_authorization = skip_authorization
+    application.save('')
+    dynreg['client_secret'] = application.client_secret
+    dynreg['grant_types'] = [
+        application.authorization_grant_type, "refresh_token"]
+    return dynreg
+
+
+class Command(BaseCommand):
+    help = 'Register an OAuth2 client/ relying party'
+
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('client_id', nargs='?')
+        parser.add_argument('client_name', nargs='?',
+                            default=str(uuid.uuid4()))
+        parser.add_argument('--redirect_uris', nargs='+',
+                            help="A required list or redirect URIS")
+        parser.add_argument('--client_secret', nargs='?', default=None)
+        parser.add_argument('--skip_authorization', nargs='?',
+                            default=False, type=int)
+
+        # Perform a delete
+        parser.add_argument(
+            '--delete',
+            action='store_true',
+            dest='delete',
+            help='Delete application instead of closing it.',
+        )
+
+    def handle(self, *args, **options):
+        if options['delete']:
+            delete_app(options['client_id'])
+            self.stdout.write("%s application deleted." %
+                              (options['client_id']))
+        else:
+
+            dynreg = register_app(options['client_id'], options['client_name'],
+                                  options['redirect_uris'], options['skip_authorization'])
+            self.stdout.write(json.dumps(dynreg, indent=4))
