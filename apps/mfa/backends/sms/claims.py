d2ebff942c580e12a8824fd2e6028e0991e90229
Alan Viars <aviars@videntity.com>
2021-01-10 15:24:32 -0500

    amr claim support. plus vot bugfix

--- apps/mfa/backends/sms/claims.py
+++ apps/mfa/backends/sms/claims.py
@@ -16,6 +16,18 @@ class AuthenticatorAssuranceProvider(BaseProvider):
         except Exception:
             return None
 
+    def claim_amr(self):
+        try:
+            # we can say that if a user has a device
+            # it was used durring authentication as
+            # that is currently enforced. This may not
+            # hold up if changes are made to the authentication
+            # flow of the system.
+            if has_sms_device(self.user):
+                return ["pwd", "sms", ]
+        except Exception:
+            return None
+
     def claim_vot(self):
         # https://tools.ietf.org/html/rfc8485#appendix-A.2
         # Ce could also be Cf
@@ -29,7 +41,7 @@ class AuthenticatorAssuranceProvider(BaseProvider):
                 vot = self.claims.get("vot", False)
                 if vot:
                     parts = vot.split(".")
-                    parts[1] = "Ce"
+                    parts[1] = "C2"
                     return ".".join(parts)
                 return None
         except Exception:
