6597ce0fd20ef6ee17bac74ff41b7440597cdb27
Alan Viars <aviars@videntity.com>
2021-02-22 13:03:44 -0500

    fix dynreg

--- apps/dynamicreg/management/commands/register_oauth2_client.py
+++ apps/dynamicreg/management/commands/register_oauth2_client.py
@@ -20,27 +20,53 @@ def delete_app(client_id):
             "Application %s not deleted because it doesn't exist." % (client_id))
 
 
-def register_app(client_id, client_name, redirect_uris=[],
-                 skip_authorization=False, username=None):
+def register_app(client_name, client_id=None,
+                 client_secret=None,
+                 redirect_uris=[],
+                 client_type='confidential',
+                 grant_type='authorization-code',
+                 skip_authorization=None, username=None):
 
     dynreg = OrderedDict()
     dynreg['client_name'] = client_name
+    if not client_id:
+        client_id = str(uuid.uuid4())
+    if not client_secret:
+        client_secret = str(uuid.uuid4())
+    if not skip_authorization:
+        skip_authorization = False
+
     dynreg['client_id'] = client_id
+    dynreg['client_secret'] = client_secret
     dynreg['redirect_uris'] = redirect_uris
-    application, g_o_c = Application.objects.get_or_create(
-        client_id=dynreg['client_id'])
-    application.name = dynreg['client_name']
-    application.redirect_uris = dynreg['redirect_uris']
-    application.client_type = 'confidential'
-    application.authorization_grant_type = 'authorization-code'
-    application.skip_authorization = skip_authorization
-    application.save()
+    dynreg['client_type'] = client_type
+    dynreg['reasponse_types'] = ['code', 'id_token']
+    dynreg['grant_types'] = ['authorization_code', 'refresh_token']
+
+    if Application.objects.filter(client_id=client_id).exists():
+        application = Application.objects.get(client_id=client_id)
+        application.client_secret = client_secret
+        application.name = client_name
+        application.skip_authorization = skip_authorization
+        application.redirect_uris = ' '.join(redirect_uris)
+        application.client_type = client_type
+        application.authorization_grant_type = grant_type
+        application.save()
+
+    else:
+        application = Application.objects.create(client_id=client_id,
+                                                 client_secret=client_secret,
+                                                 name=client_name,
+                                                 skip_authorization=skip_authorization,
+                                                 redirect_uris=' '.join(redirect_uris),
+                                                 client_type='confidential',
+                                                 authorization_grant_type='authorization-code')
 
     if username:
         user = User.objects.get(username=username)
         application.user = user
         application.save()
-    dynreg['client_secret'] = application.client_secret
+
     dynreg['grant_types'] = [
         application.authorization_grant_type, "refresh_token"]
     return dynreg
@@ -51,12 +77,14 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         # Positional arguments
-        parser.add_argument('client_id', nargs='?')
         parser.add_argument('client_name', nargs='?',
                             default=str(uuid.uuid4()))
+        parser.add_argument('client_id', nargs='?')
         parser.add_argument('--redirect_uris', nargs='+',
-                            help="A required list or redirect URIS")
-        parser.add_argument('--client_secret', nargs='?', default=None)
+                            help="A required list or redirect URIS", default=[])
+        parser.add_argument('--client_secret', default=None)
+        parser.add_argument('--client_type', default='confidential')
+        parser.add_argument('--grant_type', default='authorization_code')
         parser.add_argument('--skip_authorization', nargs='?',
                             default=False, type=int)
         parser.add_argument('--username', nargs='?', default=None)
@@ -76,8 +104,12 @@ class Command(BaseCommand):
                               (options['client_id']))
         else:
 
-            dynreg = register_app(options['client_id'], options['client_name'],
+            dynreg = register_app(options['client_name'],
+                                  options['client_id'],
+                                  options['client_secret'],
                                   options['redirect_uris'],
+                                  options['client_type'],
+                                  options['grant_type'],
                                   options['skip_authorization'],
                                   options['username'],
                                   )
