3796140f59a243b9500f1bd0aaae1f2acea173b8
Alan Viars <aviars@videntity.com>
2021-02-22 13:44:25 -0500

    Dynreg2 (#328)
    * cleanup
    
    * fix dynreg
    
    * DB updates
    
    * Updated supporting libs and security patch
    
    * Updated supporting libs and minor bugfixes
    
    * flake8
    
    * adjusted code to use the update pyjwt library

--- apps/dynamicreg/views.py
+++ apps/dynamicreg/views.py
@@ -26,9 +26,35 @@ def registration_endpoint(request):
                     user = authenticate(username=uname, password=passwd)
                     if user is not None and user.is_active:
                         if user.groups.filter(name='DynamicClientRegistrationProtocol').exists():
-                            response = register_app(client_id=body['client_id'],
-                                                    client_name=body['client_name'],
-                                                    redirect_uris=' '.join(body['redirect_uris']),
+                            client_id = None
+                            if 'client_id' in body.keys():
+                                client_id = body['client_id']
+                            client_secret = None
+                            if 'client_secret' in body.keys():
+                                client_secret = body['client_secret']
+
+                            skip_authorization = False
+                            if 'skip_authorization' in body.keys():
+                                skip_authorization = body['skip_authorization']
+
+                            client_type = 'confidential'
+                            if 'client_type' in body.keys():
+                                client_type = body['client_type']
+
+                            redirect_uris = []
+                            if 'redirect_uris' in body.keys():
+                                redirect_uris = body['redirect_uris']
+
+                            grant_type = 'authorization_code'
+                            if 'grant_type' in body.keys():
+                                grant_type = body['grant_type']
+                            response = register_app(client_name=body['client_name'],
+                                                    client_id=client_id,
+                                                    client_secret=client_secret,
+                                                    redirect_uris=redirect_uris,
+                                                    client_type=client_type,
+                                                    grant_type=grant_type,
+                                                    skip_authorization=skip_authorization,
                                                     username=user.username)
                             return JsonResponse(response)
                         else:
